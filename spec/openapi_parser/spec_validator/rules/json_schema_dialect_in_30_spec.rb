bf3ce4d99bd6ff9c6506ffc5c5e62fa980bf2f0b
fusagiko / takayamaki <takayamaki@users.noreply.github.com>
2026-05-20 01:45:53 +0900

    Add pending specs for jsonSchemaDialect parse + rule (3.1 strategy PR11)

---
+++ spec/openapi_parser/spec_validator/rules/json_schema_dialect_in_30_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../../../spec_helper'
+
+RSpec.describe 'OpenAPIParser::SpecValidator::Rules::JsonSchemaDialectIn30' do
+  def base_doc(openapi_version_string)
+    {
+      'openapi' => openapi_version_string,
+      'info' => { 'title' => 'test', 'version' => '1.0' },
+      'paths' => {},
+    }
+  end
+
+  def doc_with_dialect(openapi_version_string)
+    raw = base_doc(openapi_version_string)
+    raw['jsonSchemaDialect'] = 'https://spec.openapis.org/oas/3.1/dialect/base'
+    OpenAPIParser.parse(raw, strict_reference_validation: false)
+  end
+
+  def doc_without_dialect(openapi_version_string)
+    OpenAPIParser.parse(base_doc(openapi_version_string), strict_reference_validation: false)
+  end
+
+  def run_rule_for(root)
+    OpenAPIParser::SpecValidator::Rules::JsonSchemaDialectIn30.new(root.openapi_version).check(root)
+  end
+
+  context 'with a 3.1 document declaring jsonSchemaDialect' do
+    it 'reports no violation'
+  end
+
+  context 'with a 3.1 document without jsonSchemaDialect' do
+    it 'reports no violation'
+  end
+
+  context 'with a 3.0 document declaring jsonSchemaDialect' do
+    it 'reports one violation pointing at #/jsonSchemaDialect'
+  end
+
+  context 'with a 3.0 document without jsonSchemaDialect' do
+    it 'reports no violation'
+  end
+
+  context 'with an :unknown version document' do
+    it 'reports no violation (rule skipped)'
+  end
+end
+
+RSpec.describe 'OpenAPI#json_schema_dialect parse layer' do
+  let(:root) do
+    raw = {
+      'openapi' => '3.1.0',
+      'info' => { 'title' => 'test', 'version' => '1.0' },
+      'paths' => {},
+      'jsonSchemaDialect' => 'https://spec.openapis.org/oas/3.1/dialect/base',
+    }
+    OpenAPIParser.parse(raw, strict_reference_validation: false)
+  end
+
+  it 'exposes the dialect URI string'
+end
